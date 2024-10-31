d3de7efa409bdb1181e4f26d628b0ae0c4e82a63
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2024-10-31 13:38:55 -0600

    adding the stm32-default uart comment project

---
+++ Projects/stm32-default uart cmments/src/main.c
@@ -0,0 +1,38 @@
+#include "stm32f4xx_hal.h"
+#include <stdio.h>
+
+UART_HandleTypeDef huart2;
+
+void UART_Init(void) {
+    __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3; // PA2 -> TX, PA3 -> RX
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&huart2);
+}
+
+int main(void) {
+    HAL_Init();
+    UART_Init();
+
+    while (1) {
+        char msg[] = "Status: Running...\n";
+        HAL_UART_Transmit(&huart2, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+        HAL_Delay(1000); // Delay 1 second
+    }
+}
